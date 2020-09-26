a733134aca47b0b467af5478fcfe8700d7abed57
Ravi1202 <70045328+Ravi1202@users.noreply.github.com>
2020-09-25 19:49:06 -0700

    Add files via upload

---
+++ AngryBirdsStage3.5-master/AngryBirdsStage3.5-master/Slingshot.js
@@ -0,0 +1,21 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointA=pointB,
+        this.slings = Constraint.create(options);
+        World.add(world, this.slings);
+    }
+
+    display(){
+        var pointA = this.slings.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
